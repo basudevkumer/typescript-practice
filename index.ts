b7aad6825d063557e86e36e841b652d9636ec760
basudevkumar <jhulonkumar40@gmail.com>
2026-03-28 19:49:54 +0600

    practice :- how to write object in typescript (basic)

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-const map2: Record<string, number> = {};
+const car: { type: string; model: string; year: number } = {
+  type: "BWM",
+  model: "Corolla",
+  year: 1999,
+};
 
-map2["Jack"] = 25;
-map2.BK =  58
-
-console.log(map2);
+console.log(car["year"]);
