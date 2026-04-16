1a7a39664e5c10d72c0cea3bd735958eb592f09f
basudevkumar <jhulonkumar40@gmail.com>
2026-04-16 10:08:00 +0600

    Add TypeScript classes notes

--- index.ts
+++ index.ts
@@ -1,12 +1,104 @@
-// let sameValue : unknown = "sdfsdfsdf"
+// class Person {
+//   public name: string;
 
-// let valueLength : number = (sameValue as string).length
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
 
-// console.log(valueLength);
+// const p1 = new Person("JK Roy");
 
+// console.log(p1.name);
 
-// const jsonString = '{"name": "Rahim", "age": 25}';
-// const parsed = JSON.parse(jsonString) as { name: string; age: number };
+// class Person{
 
-// console.log(parsed.name); 
-// console.log(parsed.age);  
+//   private name : string ;
+
+//   constructor(name:string){
+//      this.name =  name
+//   }
+
+//   getName():string{
+//     return this.name
+//   }
+
+// }
+
+//  const p1 =  new Person("JK Roy")
+
+//  console.log(p1.getName());
+
+// class Father {
+//   protected taka: number = 5000;
+// }
+
+// class Soon extends Father {
+//   show(){
+//     console.log(this.taka);
+
+//   }
+// }
+
+// const taka1 =  new Soon()
+
+// taka1.show()
+// console.log(taka1.taka);
+
+// class Person {
+//   readonly name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+
+// const p1 =  new Person("BK Roy")
+
+// p1.name = "JK ROy"
+// console.log(p1.name);
+
+// class Regtunglar {
+//   constructor(
+//     protected width: number,
+//     protected height: number,
+//   ) {}
+
+//   getArea(): number {
+//     return this.height * this.width;
+//   }
+
+// }
+
+// class Square extends Regtunglar {
+//   constructor(width: number){
+//     super(width, width)
+//   }
+// }
+
+// const r1 =  new Square(10)
+// const r2 =  new Square(100)
+
+// console.log(r1.getArea());
+// console.log(r2.getArea());
+
+abstract class Shpe {
+  abstract getArea(): number;
+
+  describe(): string {
+    return "This shapes ages = "  + this.getArea();
+  }
+}
+
+class Circle extends Shpe {
+  constructor(private radius: number) {
+    super();
+  }
+
+  getArea(): number {
+    return 3.14 * this.radius * this.radius;
+  }
+}
+
+const c = new Circle(7);
+
+console.log(c.describe());
